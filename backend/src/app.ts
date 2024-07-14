29b57cf954a5667e4e616be7b1f3da651d6f59b0
Abigail.Zhang <zzjabigail@gmail.com>
2024-07-14 19:46:53 +1000

    Create post method for search functions

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,11 @@ import fs from "fs";
 
 const app = express();
 const port = 8888;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
 
 app.use(cors());
 
@@ -27,8 +32,59 @@ app.get("/names", (req: Request, res: Response, next: NextFunction) => {
 }
 );
 
-// Functions to load all names from json file
+app.post("/search", (req: Request, res: Response, next: NextFunction) => {
+  const { input } = req.body
+  if(!input){
+    return res.status(400).send({error: "Bad Request: Invalid input request"})
+  } else {
+    const names = loadNames().names;
+    if (!names) {
+      return res.status(404).json({error:"No names found from the json file."})
+    }
+    const result = searchName(names, input)
+    return res.status(200).json({result})
+  }
+}
+);
+
+// Function to load all names from json file
 function loadNames() {
   const data = fs.readFileSync(__dirname+"/names.json", "utf8")
   return JSON.parse(data)
 }
+
+//Function to search name based on input
+function isChinese(char: string): boolean {
+  return new RegExp(/\p{Script=Han}/u).test(char)
+}
+
+function splitName(name: string): string[] {
+  let resultList: string[] = []
+  // split the string by spaces/tabs
+  const nameList = name.split(/\s+/)
+  nameList.forEach(char => {
+    // split Chinese by characters
+    if (isChinese(char)) {
+        resultList = resultList.concat(char.split(""))
+    } else {
+        resultList.push(char.toLowerCase())
+    }
+  })
+  return resultList
+}
+
+function searchName(names: string[], input:string) : string {
+  let result = ""
+
+  // split input
+  const inputCharList = splitName(input)
+
+  //loop through names.json
+  names.forEach(name => {
+    const nameList = splitName(name)
+    if (inputCharList.every(item => nameList.includes(item))) {
+      result = name
+    }
+  })
+  return result
+};
